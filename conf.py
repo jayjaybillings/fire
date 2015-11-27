8d4efc217ee3388eb3897e8ae0e323f71dcf9d0e
Jay Jay Billings <billingsjj@ornl.gov>
2015-11-27 09:56:39 -0500

    Added markdown suffix configuration in conf.py.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- conf.py
+++ conf.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python
 # Configuration file for generating docs on readthedocs.org
 import subprocess, os
+from recommonmark.parser import CommonMarkParser
 
+# Generate Doxygen files
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
     subprocess.call('doxygen Doxyfile.rtfd', shell=True)
+
+# Configure markdown support
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
