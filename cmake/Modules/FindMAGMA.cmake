55b5f139a259b2799baf398776d3f043e5a1c902
Jay Jay Billings <billingsjj@ornl.gov>
2016-09-25 18:13:42 -0400

    Fixed a bug in the appended MAGMA libraries list.
    Signed-off-by: Jay Jay Billings <billingsjj@ornl.gov>

--- cmake/Modules/FindMAGMA.cmake
+++ cmake/Modules/FindMAGMA.cmake
@@ -32,37 +32,36 @@
 # Author(s): Jay Jay Billings
 #----------------------------------------------------------------------------*/
 #
-# This file was adapted from:
-#    http://stackoverflow.com/questions/17747470/cmake-find-library-does-not-find-the-library
-#
-# I saved the header, but rewrote the rest to use PkgConfig.
-#
-# - Find the MAGMA library
+# Find the MAGMA library of hybrid CPU+GPU solvers.
 #
 # Usage:
 #   find_package(MAGMA [REQUIRED] [QUIET] )
 #
-# It sets the following variables:
-#   MAGMA_FOUND               ... true if magma is found on the system
-#   MAGMA_LIBRARY_DIRS        ... full path to magma library
-#   MAGMA_INCLUDE_DIRS        ... magma include directory
-#   MAGMA_LIBRARIES           ... magma libraries
+# The following variables will be set if the library is found:
+#   MAGMA_FOUND          ... set to true if module(s) exist
+#   MAGMA_LIBRARIES      ... list of MAGMA libraries (w/o the '-l')
+#   MAGMA_LIBRARY_DIRS   ... the paths of the libraries (w/o the '-L')
+#   MAGMA_LDFLAGS        ... all required linker flags
+#   MAGMA_LDFLAGS_OTHER  ... all other linker flags
+#   MAGMA_INCLUDE_DIRS   ... the '-I' preprocessor flags (w/o the '-I')
+#   MAGMA_CFLAGS         ... all required cflags
+#   MAGMA_CFLAGS_OTHER   ... the other compiler flags
 #
-# The following variables will be checked by the function
-#   MAGMA_USE_STATIC_LIBS     ... if true, only static libraries are found
+# The following variables can be used to influence how the function searches 
+# for MAGMA:
 #   MAGMA_ROOT                ... if set, the libraries are exclusively searched
 #                                 under this path
 
-#Reconfigure the pkgconfig path to contain the MAGMA lib directory, which
-#is where the magma.pc file is located.
+# Reconfigure the pkgconfig path to contain the MAGMA lib directory, which
+# is where the magma.pc file is located.
 set( ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${MAGMA_ROOT}/lib/pkgconfig" )
 
-#Find MAGMA using PKGCONFIG
+# Find MAGMA using PKGCONFIG
 find_package(PkgConfig REQUIRED)
 pkg_search_module(MAGMA REQUIRED magma magma_sparse)
 
 if (MAGMA_FOUND)
    message(STATUS "MAGMA libraries found in ${MAGMA_LIBRARY_DIRS}.")
-   #Modify the library list to include the magma_sparse library
-   set(MAGMA_LIBRARIES "magma_sparse:${MAGMA_LIBRARIES}")
+   # Modify the library list to include the magma_sparse library
+   set(MAGMA_LIBRARIES "magma_sparse;${MAGMA_LIBRARIES}")
 endif(MAGMA_FOUND)
